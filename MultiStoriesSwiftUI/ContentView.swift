6393cfdc972ec3975f42146533ef467277355105
Cesar Humberto <bertochgg@gmail.com>
2025-10-31 17:35:12 -0600

    Initial Commit

---
+++ MultiStoriesSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MultiStoriesSwiftUI
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 31/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MultiStoriesSwiftUI/MultiStoriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiStoriesSwiftUIApp.swift
+//  MultiStoriesSwiftUI
+//
+//  Created by Cesar Humberto Grifaldo Garcia on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MultiStoriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
